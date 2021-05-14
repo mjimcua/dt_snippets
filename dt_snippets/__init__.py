759a57487f0291f57053c383c2874d27fbf9ba9f
Miguel Ángel Jiménez Cuadrillero <miguelangel.jimenez@sage.com>
2021-05-14 12:11:06 +0200

    Commit inicial

--- dt_snippets/__init__.py
+++ dt_snippets/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ['dt_snippets']
+"""
+pyexample.
+
+An example python library.
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Miguel Ángel Jiménez Cuadrillero'
+__credits__ = 'StackOverflow'
